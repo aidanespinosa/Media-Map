3634fa12b444e1f01a2403f9aacb1c06c074fa72
bwax <braxton.waxdeck@gmail.com>
2022-12-20 19:51:19 -0800

    added controller routes started handlebars

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 
+const { engine } = require('express-handlebars');
+
+const mainRouter = require ("./controllers")
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.use(mainRouter);
 
 app.listen(PORT, () =>{
     console.log("Listening on http://localhost:" + PORT);
