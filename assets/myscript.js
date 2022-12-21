ba0b9468f9ccde7199d4c2d6eca92d2dd190a516
RayR124 <RealtorRayR@Gmail.com>
2022-12-21 12:33:39 -0800

    updated style.css and minor js changes, nothing major

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,22 +1,23 @@
 const searchButton = document.getElementById(".searchBar");
-const url = "https://api.themoviedb.org/4/list/1?api_key=<<api_key>>";
+const url = "https://api.themoviedb.org/4/list/1?api_key=a299fbe5ab099ad1c542674fd239f25d";
+const apiKey = "a299fbe5ab099ad1c542674fd239f25d";
 
 let results = {
     fetchMovie: function (movie) {
-        fetch(/*API goes here*/)
+        fetch("https://api.themoviedb.org/4/" + movie + "/1?api_key=" + apiKey)
             .then((response) => response.json()).then((data) => this.displayResults(data));
     },
 
     displayResults: function (data) {
         const { name } = data;
-        const { description } = data./*description*/
-        const { reviews } = data./*reviews*/
+        const { description } = data;
+        const { reviews } = data;
 
         document.body.style.backgroundImage = "url(\"https://source.unsplash.com/1600x900/?" + name + "\")";
     },
 
     fetchResults: function (data) {
-        fetch(/*API oes here again*/).then((response) => response.json()).then((data) => displayFeatures(data));
+        fetch("https://api.themoviedb.org/4/" + data + "/1?api_key=" + apiKey).then((response) => response.json()).then((data) => displayFeatures(data));
 
         function displayFeatures(data) {
             for (let i = 0; i < 5; i++) {
