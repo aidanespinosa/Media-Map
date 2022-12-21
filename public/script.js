2398221272095a5b1665c04bd5e5eecafb6d7592
RayR124 <RealtorRayR@Gmail.com>
2022-12-21 13:42:18 -0800

    slotted in API to JS

--- public/script.js
+++ public/script.js
@@ -1,26 +1,27 @@
 const searchButton = document.getElementById(".searchBar");
-const url = "https://api.themoviedb.org/4/list/1?api_key=<<api_key>>";
+const url = "https://api.themoviedb.org/3/movie/550?api_key=a299fbe5ab099ad1c542674fd239f25d";
+const apiKey = "a299fbe5ab099ad1c542674fd239f25d";
 
 let results = {
     fetchMovie: function (movie) {
-        fetch(/*API goes here*/)
+        fetch("https://api.themoviedb.org/3/" + movie + "movie/550?api_key=a299fbe5ab099ad1c542674fd239f25d")
             .then((response) => response.json()).then((data) => this.displayResults(data));
     },
 
     displayResults: function (data) {
         const { name } = data;
-        const { description } = data./*description*/
-        const { reviews } = data./*reviews*/
+        const { description } = data;/*description*/
+        const { reviews } = data;/*reviews*/
 
         document.body.style.backgroundImage = "url(\"https://source.unsplash.com/1600x900/?" + name + "\")";
     },
 
     fetchResults: function (data) {
-        fetch(/*API oes here again*/).then((response) => response.json()).then((data) => displayFeatures(data));
+        fetch("https://api.themoviedb.org/3/" + data + "movie/550?api_key=a299fbe5ab099ad1c542674fd239f25d").then((response) => response.json()).then((data) => displayFeatures(data));
 
         function displayFeatures(data) {
             for (let i = 0; i < 5; i++) {
-                const thumbnail = data./*thumbnail icon from API call*/
+                const thumbnail = data;/*thumbnail icon from API call*/
                 // calls the results and returns a thumbnail to each feature card, up to 6
                 document.querySelector(`.feature${i + 2}`).innerHTML = thumbnail;
             }
