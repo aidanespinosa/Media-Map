b8535107de5bf0e4476d9db190e8920793062864
aidanespinosa <aidanespinosa3@gmail.com>
2022-12-23 14:43:43 -0800

    Set up data base structure using Sequelize Orm

---
+++ models/service.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Service extends Model {}
+const sequelize = require("../config/connection");
+
+Service.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    subscription: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+  },
+  { sequelize, modelName: "service" }
+);
+
+module.exports = Service;
