ae85e00079b7566abe1fe7a0e8125aec218b3b4e
RayR124 <RealtorRayR@Gmail.com>
2022-12-22 20:56:48 -0800

    API works and calls data, data is not yet displayed in html

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const searchButton = document.getElementById(".searchBar");
-const url = "https://api.themoviedb.org/3/movie/550?api_key=a299fbe5ab099ad1c542674fd239f25d";
+const url = "https://api.themoviedb.org/3/search/movie?api_key=a299fbe5ab099ad1c542674fd239f25d&language=en-US&page=1&include_adult=false";
 const apiKey = "a299fbe5ab099ad1c542674fd239f25d";
 const readAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjk5ZmJlNWFiMDk5YWQxYzU0MjY3NGZkMjM5ZjI1ZCIsInN1YiI6IjYzYTI4ZjU5MmYzYjE3MDA4NWRkNDM0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Uixy0unpQnM8ymcpZ71z1YTcv_PwBCSnK81qGl6PZ5Q";
 
@@ -59,34 +59,34 @@ function toggleSignupModal() {
 
 let results = {
     fetchMovie: function (movie) {
-        fetch("https://api.themoviedb.org/3/" + movie + "/550?api_key=a299fbe5ab099ad1c542674fd239f25d")
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&page=1&include_adult=false&query=" + movie)
             .then((response) => response.json()).then((data) => this.displayResults(data));
     },
 
     displayResults: function (data) {
-        const { name } = data;
-        const { description } = data;/*description*/
-        const { reviews } = data;/*reviews*/
-
-        document.body.style.backgroundImage = "url(\"https://source.unsplash.com/1600x900/?" + name + "\")";
+        data = data.results[0];
+        let name = data.original_title;
+        let description = data.overview;/*description*/
+        let reviews = data.vote_average;/*reviews*/
+        console.log(data);
+        document.body.style.backgroundImage = "url(\"https://source.unsplash.com/1600x900/?movie&query=" + name + ")";
     },
 
-    fetchResults: function (data) {
-        fetch("https://api.themoviedb.org/3/" + data + "movie/550?api_key=a299fbe5ab099ad1c542674fd239f25d").then((response) => response.json()).then((data) => displayFeatures(data));
+    /*fetchResults: function (movie) {
+        fetch("https://api.themoviedb.org/3/search/" + movie + "?" + apiKey + "&language=en-US&page=1&include_adult=false").then((response) => response.json()).then((data) => displayFeatures(data));
 
         function displayFeatures(data) {
             for (let i = 0; i < 6; i++) {
-                const thumbnail = data;/*thumbnail icon from API call*/
+                const thumbnail = data;/*thumbnail icon from API call
                 // calls the results and returns a thumbnail to each feature card, up to 6
                 document.querySelector(`.feature${i + 1}`).innerHTML = thumbnail;
             }
         }
-    },
-
+    },*/
+    
     search: function () {
-        this.fetchResults(document.querySelector(".searchBar").value);
+        this.fetchMovie(document.querySelector(".searchBar").value);
         resultsModal.classList.remove("hidden");
-        console.error("text");
     },
 };
 
