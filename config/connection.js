027f5394c3f5625d10aae4822d6049981bf433ce
CharlesCoxC3 <charles.cox.iii@aol.com>
2022-12-25 12:39:18 -0800

    connection.js update

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize (
+      process.env.DB_NAME, 
+      process.env.DB_USER, 
+      process.env.DB_PASSWORD, 
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
