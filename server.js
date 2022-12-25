e4617a01c43322bef00ac7e1e11fd5a65bfaf8e2
CharlesCoxC3 <charles.cox.iii@aol.com>
2022-12-25 11:02:41 -0800

    added code to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-
 const { engine } = require('express-handlebars');
+
+const sequelize = require('./config/connection')
 
 const mainRouter = require ("./controllers")
 
@@ -11,9 +13,13 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+
 app.use(mainRouter);
 app.use(express.static('public'));
 
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () =>{
     console.log("Listening on http://localhost:" + PORT);
-})
+    });
+});
