375a04f09f3eee8bf4f76bf5c3a8da1021cdb019
CharlesCoxC3 <charles.cox.iii@aol.com>
2022-12-25 10:57:04 -0800

    added code to user.js

---
+++ controllers/api/user.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+
+const User = require('../../models/User')
+const usersRouter = new Router();
+
+usersRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+        res.status(401).end('User not found');
+        return;
+    }
+
+    res.end();
+
+});
+
+usersRouter.post("/signup", async (req, res) => {
+    console.log(req.body);
+
+    res.end();
+
+});
+
+module.exports = usersRouter;
