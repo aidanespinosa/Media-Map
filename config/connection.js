167ba63d46fec552c5f25ed6f12501488632bf60
bwax <braxton.waxdeck@gmail.com>
2023-01-09 17:24:25 -0800

    add database url initialization for sequelize

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,25 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize (
-      process.env.DB_NAME, 
-      process.env.DB_USER, 
-      process.env.DB_PASSWORD, 
-      {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+let sequelize;
+if (process.env.DATABASE_URL) {
+  console.log("SETTING DB USING URL");
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    }
+  );
+}
+
+
 
 module.exports = sequelize;
